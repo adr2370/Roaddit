2f3eeb77a655c597b3cf6ecde0a0c01edc04fd79
Alexander Ramirez <adr2370@gmail.com>
2013-02-22 23:32:54 -0800

    Added gettting external stuff for places

--- test.php
+++ test.php
@@ -1,8 +1,43 @@
 <?php
 require_once('factual-php-driver/Factual.php');
-$factual = new Factual("syEfnIulImFpvm9vRpHyUfjLjcnu5zgd7khd9qgk","CiHOa5CQuWttJ5fdtSnXHsy4BfCbWe6GVYMtbPA7");
-$query = new FactualQuery;
-$query->search("Sushi Santa Monica");
-$res = $factual->fetch("places", $query);
-print_r($res->getData());
+
+function getCircle($x,$y,$s) {
+	$factual = new Factual("syEfnIulImFpvm9vRpHyUfjLjcnu5zgd7khd9qgk","CiHOa5CQuWttJ5fdtSnXHsy4BfCbWe6GVYMtbPA7");
+	$query = new FactualQuery;
+	$query->within(new FactualCircle($x, $y, $s));
+	$query->limit(10);
+	$query->includeRowCount();
+	$query->field("category")->search("Arts, Entertainment & Nightlife");
+	return $factual->fetch("places", $query);
+}
+function getNumSitesForPlace($id) {
+	$factual = new Factual("syEfnIulImFpvm9vRpHyUfjLjcnu5zgd7khd9qgk","CiHOa5CQuWttJ5fdtSnXHsy4BfCbWe6GVYMtbPA7");
+	$query = new FactualQuery;
+	$query->field("factual_id")->equal($id);
+	try {
+		$a = $factual->fetch("crosswalk", $query);
+		$count = 0;
+		foreach($a as $j) {
+			$count++;
+		}
+		return $count;
+	} catch (Exception $e) {
+	    return 0;
+	}
+}
+$res = getCircle(34.06018, -118.41835, 5000);
+foreach($res as $i) {
+	echo $i["name"]." - ".getNumSitesForPlace($i["factual_id"])."<br/>";
+}
 ?>
+<html>
+<head>
+</head>
+<body>
+<script type='text/javascript'>
+	var x=eval(<?php echo json_encode($res); ?>);
+	for(var i in x) {
+		console.log(x[i].name);
+	}
+</script>
+</body>
