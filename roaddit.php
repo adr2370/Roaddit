53bad1274b3a3be149403a56a1db9b4ed682d859
Alexander Ramirez <adr2370@gmail.com>
2013-02-23 14:06:36 -0800

    Added input output

--- roaddit.php
+++ roaddit.php
@@ -47,6 +47,17 @@ function sortTrips($trips) {
 	uasort($trips, 'cmp');
 	return $trips;
 }
+function alreadyThere($p,$t) {
+	foreach($t as $a) {
+		if($a->name==$p->name) return true;
+	}
+	return false;
+}
+function toUnixTimestamp($time) {
+	$extraHour=0;
+	if(substr($time,-2,2)=="PM") $extraHour=12;
+	return mktime(substr($time,-8,2)+$extraHour,substr($time,-5,2),0,substr($time,0,2),substr($time,3,2),substr($time,6,4));
+}
 function getDirections($x0,$y0,$x1,$y1) {
 	return json_decode(file_get_contents("http://maps.googleapis.com/maps/api/directions/json?origin=".$x0.",".$y0."&destination=".$x1.",".$y1."&sensor=false"));
 }
@@ -108,7 +119,7 @@ function getTripsFromPlaces($p,$dur,$maxDur,$maxCost) {
 		$time=$dur;
 		$trip=array();
 		for($j=0;$j<count($p);$j++) {
-			if(($i>>$j)%2) {
+			if(($i>>$j)%2&&!alreadyThere($p[$j][0],$trip)) {
 				$currcost=0;
 				$currtime=0;
 				$currplace=$p[$j][0];
@@ -121,39 +132,48 @@ function getTripsFromPlaces($p,$dur,$maxDur,$maxCost) {
 				array_push($trip,$currplace);
 			}
 		}
-		array_push($trip,$cost,$time);
+		array_push($trip,"",$cost,$time);
 		//if($cost<=$maxCost&&$time<=$maxDur) {
 			array_push($trips,$trip);
 		//}
 	}
 	return sortTrips($trips);
 }
-function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost) {
+function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost,$types) {
 	$types="amusement_park|aquarium|art_gallery|bar|bowling_alley|campground|casino|movie_theater|museum|night_club|park|shopping_mall|spa|stadium|zoo|natural_feature|point_of_interest";
 	$dir = getDirections($x0,$y0,$x1,$y1)->routes[0]->legs[0];
 	$p = getPlacesAlongRoute($x0,$y0,$x1,$y1,$dir,$types);
 	$trips = getTripsFromPlaces($p,$dir->duration->value,$maxDur,$maxCost);
 	
 	// print out trips
-	$count = 0;
+	$count = 10;
+	echo $count;
 	foreach($trips as $t) {
-		echo json_encode($t[count($t)-1])."<br/>";
-		$count++;
-		if($count>10) break;
+		$places=0;
+		$tripOutput="";
+		foreach($t as $place) {
+			if($place=="") break;
+			$places++;
+			$tripOutput.=",".$place->name;
+			$tripOutput.=",".$place->geometry->location->lat;
+			$tripOutput.=",".$place->geometry->location->lng;
+		}
+		echo ",".$places.$tripOutput;
+		$count--;
+		if($count<=0) break;
 	}
 }
 initArrays();
 
 // Getting variables
-$startAddress=$_GET["startAddress"];
-$endAddress=$_GET["endAddress"];
-$beginDay=$_GET["beginDay"];
-$beginTime=$_GET["beginTime"];
-$endDay=$_GET["endDay"];
-$endTime=$_GET["endTime"];
+$startAddress=$_GET["starta"];
+$endAddress=$_GET["enda"];
+$startDateTime=$_GET["startdt"];
+$endDateTime=$_GET["enddt"];
 $budget=$_GET["budget"];
 $categories=$_GET["categories"];
-
-// getEverything(34.019308,-118.494466,34.070489,-118.450438); //ROC to UCLA
-getEverything(34.070489,-118.450438,37.774940,-122.419430,$maxDur,$maxCost); //UCLA to San Fransisco
+$startCoor=getLatLong($startAddress);
+$endCoor=getLatLong($endAddress);
+$duration=toUnixTimestamp($endDateTime)-toUnixTimestamp($startDateTime);
+getEverything($startCoor->lat,$startCoor->lng,$endCoor->lat,$endCoor->lng,$duration,$budget,$categories);
 ?>
