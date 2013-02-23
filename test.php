bfb0cee80bfbf2ac0efe9d60f0d1eba8b70f3887
Alexander Ramirez <adr2370@gmail.com>
2013-02-23 00:29:15 -0800

    Added google stuff

--- test.php
+++ test.php
@@ -1,43 +1,35 @@
 <?php
-require_once('factual-php-driver/Factual.php');
-
-function getCircle($x,$y,$s) {
-	$factual = new Factual("syEfnIulImFpvm9vRpHyUfjLjcnu5zgd7khd9qgk","CiHOa5CQuWttJ5fdtSnXHsy4BfCbWe6GVYMtbPA7");
-	$query = new FactualQuery;
-	$query->within(new FactualCircle($x, $y, $s));
-	$query->limit(10);
-	$query->includeRowCount();
-	$query->field("category")->search("Arts, Entertainment & Nightlife");
-	return $factual->fetch("places", $query);
+function distance($x0,$y0,$x1,$y1) {
+	$R=6371; // in km
+	$dLat=deg2rad($y1-$y0);
+	$dLon=deg2rad($x1-$x0);
+	$lat1=deg2rad($y0);
+	$lat2=deg2rad($y1);
+	$a=sin($dLat/2) * sin($dLat/2) + sin($dLon/2) * sin($dLon/2) * cos($lat1) * cos($lat2); 
+	$c = 2 * atan2(sqrt($a), sqrt(1-$a));
+	$d = $R * $c;
+	return $d;
 }
-function getNumSitesForPlace($id) {
-	$factual = new Factual("syEfnIulImFpvm9vRpHyUfjLjcnu5zgd7khd9qgk","CiHOa5CQuWttJ5fdtSnXHsy4BfCbWe6GVYMtbPA7");
-	$query = new FactualQuery;
-	$query->field("factual_id")->equal($id);
-	try {
-		$a = $factual->fetch("crosswalk", $query);
-		$count = 0;
-		foreach($a as $j) {
-			$count++;
-		}
-		return $count;
-	} catch (Exception $e) {
-	    return 0;
-	}
+function getCircle($x,$y,$s,$type) {
+	return file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$x.",".$y."&radius=".$s."&types=".$type."&sensor=false&key=AIzaSyD3j1urj8LrNyuu5-lViawfLG6nn1N6IJg");
 }
-$res = getCircle(34.06018, -118.41835, 5000);
-foreach($res as $i) {
-	echo $i["name"]." - ".getNumSitesForPlace($i["factual_id"])."<br/>";
+function getAllPlaces($x0,$y0,$x1,$y1) {
+	$circleSize = 16000; // in meters
+	$dist = distance($x0,$y0,$x1,$y1); // in km
+	$res = getCircle(34.06018, -118.41835, $circleSize);
+}
+$res=json_decode(getCircle(34.06018, -118.41835, 500));
+foreach($res->results as $i) {
+	$name=$i->name;
+	$x=$i->geometry->location->lat;
+	$y=$i->geometry->location->lng;
+	$open=$i->opening_hours->open_now;
+	$rating=$i->rating;
+	echo $name." ".$x." ".$y." ".$open." ".$rating."<br/>";
 }
 ?>
 <html>
 <head>
 </head>
 <body>
-<script type='text/javascript'>
-	var x=eval(<?php echo json_encode($res); ?>);
-	for(var i in x) {
-		console.log(x[i].name);
-	}
-</script>
 </body>
