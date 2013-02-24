450c512a359de6024234fbcbc52ddff680675e59
Alexander Ramirez <adr2370@gmail.com>
2013-02-23 18:30:55 -0800

    Major speed improvements

--- roaddit.php
+++ roaddit.php
@@ -1,6 +1,7 @@
 <?php
 $costs = array();
 $times = array();
+$circles = array();
 function initArrays() {
 	global $costs;
 	global $times;
@@ -64,7 +65,8 @@ function getDirections($x0,$y0,$x1,$y1) {
 	return json_decode(file_get_contents("http://maps.googleapis.com/maps/api/directions/json?origin=".$x0.",".$y0."&destination=".$x1.",".$y1."&sensor=false"));
 }
 function getCircle($x,$y,$s,$type) {
-	return json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$x.",".$y."&radius=".$s."&types=".$type."&sensor=false&key=AIzaSyD3j1urj8LrNyuu5-lViawfLG6nn1N6IJg"));
+	global $circles;
+	array_push($circles,"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$x.",".$y."&radius=".$s."&types=".$type."&sensor=false&key=AIzaSyD3j1urj8LrNyuu5-lViawfLG6nn1N6IJg");
 }
 function getLatLong($address) { // getLatLong("")->lat and getLatLong("")->lng
 	return json_decode(file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($address)."&sensor=false"))->results[0]->geometry->location;
@@ -73,11 +75,13 @@ function getPhoto($photo) {
 	return file_get_contents("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=".$photo."&sensor=true&key=AIzaSyD3j1urj8LrNyuu5-lViawfLG6nn1N6IJg");
 }
 function getPlacesAlongRoute($x0,$y0,$x1,$y1,$dir,$types) {
+	global $circles;
 	$queryLimit = 10; // max number of total circles
 	$distance = $dir->distance->value;
 	$circleSize = max(min($distance/10,32000),3200);
 	$duration = $dir->duration->value;
-	$distSoFar = $circleSize*2;
+	$distLimit = $circleSize*3;
+	$distSoFar = $distLimit;
 	$timeSoFar = 0;
 	$places = array();
 	foreach($dir->steps as $s) {
@@ -86,7 +90,7 @@ function getPlacesAlongRoute($x0,$y0,$x1,$y1,$dir,$types) {
 		$x3=$s->end_location->lat;
 		$y3=$s->end_location->lng;
 		$distSoFar += $s->distance->value;
-		if($distSoFar>=$circleSize*2) {
+		if($distSoFar>=$distLimit) {
 			$distSoFar=0;
 			$stepDist=($s->distance->value)/$circleSize;
 			if($stepDist>5) $stepDist=5;
@@ -94,30 +98,49 @@ function getPlacesAlongRoute($x0,$y0,$x1,$y1,$dir,$types) {
 			$dy=($y3-$y2)/($stepDist+1);
 			$dt=$s->duration->value/($stepDist+1);
 			for($i=0;$i<=$stepDist;$i++) {
-				if($queryLimit > 0) {
-					$res = getCircle($x2+$i*$dx, $y2+$i*$dy, $circleSize, $types);
-					$food = getCircle($x2+$i*$dx, $y2+$i*$dy, $circleSize, "food");
-					$queryLimit--;
-					$curr = array();
-					foreach($res->results as $r) {
-						//$r->geometry->location
-						//$r->name
-						//getPhoto($r->photos[0]->photo_reference);
-						//$r->types
-						//$r->opening_hours->open_now
-						array_push($curr,$r);
-						foreach($food->results as $f) {
-							array_push($curr,$f);
-							array_push($curr,$timeSoFar+$i*$dt);
-							break;
-						}
-						break;
-					}
-					array_push($places,$curr);
-				}
+				$res = getCircle($x2+$i*$dx, $y2+$i*$dy, $circleSize, $types);
+				$food = getCircle($x2+$i*$dx, $y2+$i*$dy, $circleSize, "food");
 			}
 		}
+	}
+	$urls = $circles;
+	$url_count = count($urls);
+	$curl_array = array();
+	$ch = curl_multi_init();
+	foreach($urls as $count => $url) {
+		$curl_array[$count] = curl_init($url);
+		curl_setopt($curl_array[$count], CURLOPT_RETURNTRANSFER, 1);
+		curl_multi_add_handle($ch, $curl_array[$count]);
+	}
+	do {
+		curl_multi_exec($ch, $exec);
+	} while($exec > 0);
+	$numCircles=0;
+	foreach($urls as $count => $url) {
+		if($count%2==1) continue;
+		$res = json_decode(curl_multi_getcontent($curl_array[$count]));
+		$food = json_decode(curl_multi_getcontent($curl_array[$count+1]));
+		$curr = array();
+		foreach($res->results as $r) {
+			array_push($curr,$r);
+			foreach($food->results as $f) {
+				array_push($curr,$f);
+				array_push($curr,$timeSoFar+$i*$dt);
+				break;
+			}
+			break;
+		}
+		array_push($places,$curr);
 		$timeSoFar += $s->duration->value;
+		$numCircles++;
+		if($numCircles>100) break;
+	}
+	foreach($urls as $count => $url) {
+		curl_multi_remove_handle($ch, $curl_array[$count]);
+	}
+	curl_multi_close($ch); 
+	foreach($urls as $count => $url) {
+		curl_close($curl_array[$count]);
 	}
 	return $places;
 }
@@ -125,43 +148,55 @@ function getTripsFromPlaces($p,$dur,$maxDur,$maxCost,$startTime) {
 	global $costs;
 	global $times;
 	$trips=array();
-	for($i=0;$i<pow(2,count($p));$i++) {
-		$cost=0;
-		$time=$dur;
-		$trip=array();
-		$lunchDone=0;
-		$dinnerDone=0;
-		for($j=0;$j<count($p);$j++) {
-			$which=mt_rand()%2;
-			if(($i>>$j)%2&&!alreadyThere($p[$j][$which],$trip)) {
-				if($which==1) {
-					//check to make sure food is coming at right time
-					$currTime=$startTime+$p[$j][2];
-					$hour=date("G",$currTime);
-					if($hour>=12&&$hour<=14) {
-						if($lunchDone) break;
-						$lunchDone++;
-					} else if($hour>=17&&$hour<=20) {
-						if($dinnerDone) break;
-						$dinnerDone++;
-					}
-				}
-				$currcost=0;
-				$currtime=0;
-				$currplace=$p[$j][$which];
-				for($k=0;$k<count($currplace->types);$k++) {
-					$currcost=max($currcost,$costs[$currplace->types[$k]]);
-					$currtime=max($currtime,3600*$times[$currplace->types[$k]]);
-				}
-				$cost+=$currcost;
-				$time+=$currtime;
-				array_push($trip,$currplace);
+	$keys=array_keys($p);
+	for($i=0;$i<1000;$i++) {
+		shuffle($keys);
+		for($j=0;$j<5;$j++) {
+			$currPlaces=array();
+			$cost=0;
+			$time=$dur;
+			$trip=array();
+			$count=0;
+			$lunchDone=0;
+			$dinnerDone=0;
+			$currcost=0;
+			$currtime=0;
+			for($k=0;$k<=$j;$k++) {
+				array_push($currPlaces,$keys[$k]);
 			}
-		}
-		$weight=$time+$lunchDone*5+$dinnerDone*5;
-		array_push($trip,"",$cost,$time,$weight);
-		if($cost<=$maxCost&&$time<=$maxDur) {
-			array_push($trips,$trip);
+			sort($currPlaces);
+			for($k=0;$k<=$j;$k++) {
+				$which=mt_rand()%2;
+				$currplace=$p[$currPlaces[$k]][$which];
+				if(!alreadyThere($currplace,$trip)) {
+					if($which==1) {
+						//check to make sure food is coming at right time
+						$currTime=$startTime+$currtime+$p[$j][2];
+						$hour=date("G",$currTime);
+						if($hour>=12&&$hour<=14) {
+							if($lunchDone) break;
+							$lunchDone++;
+						} else if($hour>=17&&$hour<=20) {
+							if($dinnerDone) break;
+							$dinnerDone++;
+						}
+					}
+					$currcost=0;
+					$currtime=0;
+					for($k=0;$k<count($currplace->types);$k++) {
+						$currcost=max($currcost,$costs[$currplace->types[$k]]);
+						$currtime=max($currtime,3600*$times[$currplace->types[$k]]);
+					}
+					$cost+=$currcost;
+					$time+=$currtime;
+					array_push($trip,$currplace);
+				}
+			}
+			if($cost<$maxCost&&$time<$maxDur) {
+				$weight=$time+$lunchDone*5+$dinnerDone*5;
+				array_push($trip,"",$cost,$time,$weight);
+				array_push($trips,$trip);
+			}
 		}
 	}
 	return sortTrips($trips);
@@ -180,6 +215,7 @@ function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost,$types,$startTime) {
 	// print out trips
 	$count = 1;
 	echo $count;
+	$tripsGiven=array();
 	foreach($trips as $t) {
 		$places=0;
 		$tripOutput="|Start|".$x0."|".$y0;
@@ -191,8 +227,11 @@ function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost,$types,$startTime) {
 			$tripOutput.="|".$place->geometry->location->lng;
 		}
 		$tripOutput.="|End|".$x1."|".$y1;
-		echo "|".$places.$tripOutput;
-		$count--;
+		if(!in_array($tripOutput,$tripsGiven)) {
+			array_push($tripsGiven,$tripOutput);
+			echo "|".$places.$tripOutput;
+			$count--;
+		}	
 		if($count<=0) break;
 	}
 }
@@ -207,4 +246,10 @@ $startCoor=getLatLong($startAddress);
 $endCoor=getLatLong($endAddress);
 $duration=toUnixTimestamp($endDateTime)-toUnixTimestamp($startDateTime);
 getEverything($startCoor->lat,$startCoor->lng,$endCoor->lat,$endCoor->lng,$duration,$budget,$categories,toUnixTimestamp($startDateTime));
+
+	//$r->geometry->location
+	//$r->name
+	//getPhoto($r->photos[0]->photo_reference);
+	//$r->types
+	//$r->opening_hours->open_now
 ?>
