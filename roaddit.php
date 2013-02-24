ebe3bb187820599e7249f2e92f7116fedc143cbf
Alexander Ramirez <adr2370@gmail.com>
2013-02-24 09:11:06 -0800

    Removed randomization

--- roaddit.php
+++ roaddit.php
@@ -155,9 +155,9 @@ function getPlacesAlongRoute($x0,$y0,$x1,$y1,$dir,$types) {
 			$food = json_decode(curl_multi_getcontent($curl_array[$count+1]));
 			$hotel = json_decode(curl_multi_getcontent($curl_array[$count+2]));
 			$curr = $places[$count/3];
-			array_push($curr,$res->results[rand()%3]);
-			array_push($curr,$food->results[rand()%3]);
-			array_push($curr,$hotel->results[rand()%3]);
+			array_push($curr,$res->results[0]);
+			array_push($curr,$food->results[0]);
+			array_push($curr,$hotel->results[0]);
 			$places[$count/3]=$curr;
 			$timeSoFar += $s->duration->value;
 			$numCircles++;
