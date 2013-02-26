d11e04724874e559f8937cca19b6504230607fdb
Alexander Ramirez <adr2370@gmail.com>
2013-02-25 18:54:19 -0800

    Changed start and end image

--- roaddit.php
+++ roaddit.php
@@ -267,7 +267,7 @@ function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost,$types,$startTime) {
 	} else {
 		foreach($trips as $t) {
 			$places=0;
-			$tripOutput="|Start|".$x0."|".$y0."|0|0|http://www.superclass.us/sitebuilder/images/yellow_box-618x547.jpg";
+			$tripOutput="|Start|".$x0."|".$y0."|0|0|Start";
 			foreach($t as $place) {
 				if($place=="") break;
 				$places++;
@@ -291,9 +291,8 @@ function getEverything($x0,$y0,$x1,$y1,$maxDur,$maxCost,$types,$startTime) {
 					}
 				}
 				$tripOutput.="|".$tripType;
-				//$tripOutput.="|".getFormattedAddress($place->geometry->location->lat,$place->geometry->location->lng);
 			}
-			$tripOutput.="|End|".$x1."|".$y1."|0|0|http://www.superclass.us/sitebuilder/images/yellow_box-618x547.jpg|";
+			$tripOutput.="|End|".$x1."|".$y1."|0|0|End|";
 			if(!in_array($tripOutput,$tripsGiven)) {
 				array_push($tripsGiven,$tripOutput);
 				echo "|".$places.$tripOutput;
